1ce7ef9b8329f922bde65b6bfe7be5f9b3a0b20f
DubeyJi03 <dubeyanurag3925@gmail.com>
2025-09-19 17:21:57 +0530

    do it

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:9000", // 👈 backend running port
+      "/api": "http://localhost:3000", // 👈 backend running port
     },
   },
 })
